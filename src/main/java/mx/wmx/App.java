f458825ecbee26efd0b06c44752621ed9a1b2038
alex32bit <alex.macias.leon@hotmail.com>
2024-06-01 07:34:19 -0600

    settings moved to the top

--- src/main/java/mx/wmx/App.java
+++ src/main/java/mx/wmx/App.java
@@ -22,14 +22,24 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-
 public class App {
 	
+	//Log file setting
     private static final String LOG_FILE_PATH = "/var/log/nginx/access.log";
+    private static final String LOG_DATE_FORMAT = "dd/MMM/yyyy:HH:mm:ss Z";
+    
+    //Database settings
+    private static final String DB_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    
+    //connection settings
     private static final String DB_URL = "jdbc:mysql://localhost:3306/logiberto";
     private static final String DB_USER = "user1";
     private static final String DB_PASSWORD = "password1";
     
+    //SQL INSERT
+    private static final String DB_SQL_INSERT_LOG_LINE = "INSERT INTO access_log (ip_address, datetime, http_status_code,http_method, resource, user_agent) VALUES (?,?,?,?,?,?)";
+    
+    //Text file pointer
     private static long lastFilePosition = 0;
 
     public static void main(String[] args) {
@@ -95,7 +105,6 @@ public class App {
             String httpMethod = matcher.group(4);
             String requestUri = matcher.group(5);
             String userAgent = matcher.group(6);
-            String LOG_DATE_FORMAT = "dd/MMM/yyyy:HH:mm:ss Z";
             
             // Parse log Date
             SimpleDateFormat sdf = new SimpleDateFormat(LOG_DATE_FORMAT);
@@ -106,11 +115,11 @@ public class App {
             } catch(ParseException e) {
             	System.err.println("Ocurrio un error al parsear la fecha" + e);
             }
-            String DB_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss"; 
+             
             // Parse log Date
             SimpleDateFormat db_sdf = new SimpleDateFormat(DB_DATE_FORMAT);
             String dbFormattedString = db_sdf.format(datetime);
-            String sql = "INSERT INTO access_log (ip_address, datetime, http_status_code,http_method, resource, user_agent) VALUES (?,?, ?, ?, ?, ?)";
+            String sql = DB_SQL_INSERT_LOG_LINE;
             try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
                  PreparedStatement pstmt = conn.prepareStatement(sql)) {
                 pstmt.setString(1, ipAddress);
